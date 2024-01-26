85398247ad51af957daacd0d491908ca2464ce67
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-01-26 22:09:32 +0900

    バリデーション・アソシエーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+  validates :name
+  validates :description
+  validates :category_id
+  validates :condition_id
+  validates :shipping_fee_id
+  validates :shipping_area_id
+  validates :shipping_day_id
+  validates :price
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
 end
