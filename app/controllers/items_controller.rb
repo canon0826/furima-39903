0e87f37fb170f2180b763f8a5a30e058fe109e40
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-01-27 20:59:09 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+
   private
+  end
 
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
