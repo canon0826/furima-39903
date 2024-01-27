01fbcb1ecdf25fb5d89eb8e2af29f0eabd54ebc3
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-01-27 20:58:04 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,19 +3,21 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-  validates :name
-  validates :description
-  validates :category_id
-  validates :condition_id
-  validates :shipping_fee_id
-  validates :shipping_area_id
-  validates :shipping_day_id
-  validates :price
-
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    validates :price
+  end
+  
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
     validates :shipping_fee_id
     validates :shipping_area_id
     validates :shipping_day_id
+  end
 end
