87f1cf0dfd8eefd4d17472bfe3f85501edd4120a
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-02-02 18:38:24 +0900

    レビュー依頼　再修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,22 +28,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Description can't be blank")
     end
     it 'カテゴリーの情報が空欄だと出品できない' do
-      @item.category_id = nil
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
     it '商品の状態の情報が空欄だと出品できない' do
-      @item.condition_id = nil
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
     end
     it '配送料の負担の情報が空欄だと出品できない' do
-      @item.shipping_fee_id = nil
+      @item.shipping_fee_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
     end
       it '発送元の地域の情報が空欄だと出品できない' do
-      @item.shipping_area_id = nil
+      @item.shipping_area_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area can't be blank")
     end
