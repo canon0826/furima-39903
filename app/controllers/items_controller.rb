8a6ad0f46cd1a0532c40c8ebe776baf1af31e46b
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-02-06 18:18:46 +0900

    editアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,6 +20,13 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
     end
   end
 
