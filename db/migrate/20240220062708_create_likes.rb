4bd2030d56c565c7674906f2feeca67d2b508d50
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-02-22 20:06:41 +0900

    likeモデル作成

---
+++ db/migrate/20240220062708_create_likes.rb
@@ -0,0 +1,9 @@
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.bigint :user_id, null: false, foreign_key: true
+      t.bigint :tweet_id, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
