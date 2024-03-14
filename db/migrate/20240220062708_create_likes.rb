0ec194046d1a7f3137541d52703bc2dae420b79f
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-03-14 21:03:42 +0900

    カラム削除 tweet

--- db/migrate/20240220062708_create_likes.rb
+++ db/migrate/20240220062708_create_likes.rb
@@ -2,7 +2,6 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.bigint :user_id, null: false, foreign_key: true
-      t.bigint :tweet_id, null: false, foreign_key: true
       t.timestamps
     end
   end
