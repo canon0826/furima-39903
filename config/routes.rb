7d95348af513aba848f2933592d25286cdb3d329
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-03-14 21:27:30 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :items do
-  resources :orders, only: [:index, :create]
+    resources :orders, only: [:index, :create]
+      resource :likes, only: [:create, :destroy]
+    end
   end
-end
