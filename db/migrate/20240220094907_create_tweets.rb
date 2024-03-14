2645ecf38f067133067290d8b9fde154a5888f07
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-03-14 20:58:10 +0900

    tweet model削除

--- db/migrate/20240220094907_create_tweets.rb
+++
@@ -1,9 +0,0 @@
-class CreateTweets < ActiveRecord::Migration[7.0]
-  def change
-    create_table :tweets do |t|
-      t.references :user, foreign_key: true
-      t.text :text
-      t.timestamps
-    end
-  end
-end
