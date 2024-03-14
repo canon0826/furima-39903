f01d943f0cd48669f8e402776cf9b97e514a78bf
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-03-14 21:10:19 +0900

    アソシエーション　追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many:purchases
   has_many:items
+  has_many :likes
 
   attribute :name, :string
   def birthday=(value)
