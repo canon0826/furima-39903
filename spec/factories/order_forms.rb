c13f72fe069eb97dc7129c132dbf90b204f06fc6
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-03-22 21:16:47 +0900

    FactoryBot area_idを更新

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_form do
 
     postal_code { '123-4567' }
-    shipping_area_id { 1 }
+    shipping_area_id { 2 }
     city { '横浜市緑区' }
     address { '1-1' }
     building_name { '柳ビル103' }
